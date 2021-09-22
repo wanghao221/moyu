814f7c6415bc197f4afb539a548f7726a08509c0
Haiyong <59255587+wanghao221@users.noreply.github.com>
2021-09-22 01:39:53 -0500

    Add files via upload

---
+++ 游戏-飞机躲避障碍/js/script.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+            (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?a9430a37066911650e26adadcc42798a";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+            })();
