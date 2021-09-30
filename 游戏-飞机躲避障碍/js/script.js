f5c018a5f49ea8e6133019492e32dd9c175c57c3
Haiyong <59255587+wanghao221@users.noreply.github.com>
2021-09-30 10:49:09 +0800

    Delete 游戏-飞机躲避障碍 directory

--- 游戏-飞机躲避障碍/js/script.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-            (function() {
-            var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?a9430a37066911650e26adadcc42798a";
-            var s = document.getElementsByTagName("script")[0]; 
-            s.parentNode.insertBefore(hm, s);
-            })();
