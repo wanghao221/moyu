4a874b44e84103e8fef161de15eec9ab17a090c0
Haiyong <59255587+wanghao221@users.noreply.github.com>
2021-09-30 10:48:53 +0800

    Delete 游戏-打地鼠 directory

--- 游戏-打地鼠/js/script.js
+++
@@ -1,7 +0,0 @@
-//无JS
-import { quotes } from "http://haiyong.site/wp-includes/js/quotes.js";
-let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-console.log(
-  `%c✨ ${randomQuote}`,
-  "font-size:20px; background:#FFF; color:#581845;padding:10px; border: 3px solid #581845;border-radius:10px;"
-);
